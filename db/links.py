d1a51b36631797b5aff6b00e60a4e3ffe705552f
XanderWatson <saahil_bhavsar@outlook.com>
2021-04-06 20:37:18 +0530

    Added lecture link command

---
+++ db/links.py
@@ -0,0 +1,15 @@
+import mongoengine
+import datetime
+
+class Link(mongoengine.Document):
+    _branch = mongoengine.StringField()
+    ics = mongoengine.URLField()
+    em = mongoengine.URLField()
+    emtut = mongoengine.URLField()
+    maths = mongoengine.URLField()
+    mathstut = mongoengine.URLField()
+    icslab1 = mongoengine.URLField()
+    icslab2 = mongoengine.URLField()
+    icslab3 = mongoengine.URLField()
+    _reg_date = mongoengine.DateTimeField(default=datetime.datetime.now())
+    
