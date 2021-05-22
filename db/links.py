2481f022d510f51886f93717ab88859699deb111
XanderWatson <saahil_bhavsar@outlook.com>
2021-05-22 11:29:52 +0530

    Debug

--- db/links.py
+++ db/links.py
@@ -3,6 +3,6 @@ import mongoengine
 
 class Link(mongoengine.Document):
     _branch = mongoengine.StringField()
-    _ph = mongoengine.URLField(default='')
-    _cy = mongoengine.URLField(default='')
-    _ss = mongoengine.URLField(default='')
+    _ph = mongoengine.URLField(default='https://classroom.google.com')
+    _cy = mongoengine.URLField(default='https://classroom.google.com/')
+    _ss = mongoengine.URLField(default='https://classroom.google.com')
